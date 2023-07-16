d87adb91fbe76eb3d4a3ce8edb93ef46448ee327
zena <zena@tokamak.network>
2023-07-16 22:39:47 +0900

    deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,18 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import  hh from "@nomiclabs/hardhat-ethers";
+
 
 // import TitanNFTProxyJson  from "../artifacts/contracts/TitanNFTProxy.sol/TitanNFTProxy.json"
 import { TitanNFTProxy } from '../typechain-types/contracts/TitanNFTProxy'
+import { FirstEvent } from '../typechain-types/contracts/FirstEvent.sol/FirstEvent'
 
 const deployTitanNFT: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     console.log('deployL2 hre.network.config.chainId', hre.network.config.chainId)
     console.log('deployL2 hre.network.name', hre.network.name)
 
-    const { deployer } = await hre.getNamedAccounts();
-    // console.log('deployer', deployer)
-    // console.log('hre.ethers.provider', hre.ethers.provider)
+    const { deployer, tonAddress} = await hre.getNamedAccounts();
+    console.log('deployer', deployer)
+    console.log('tonAddress', tonAddress)
 
     const { deploy, deterministic } = hre.deployments;
 
@@ -20,6 +21,11 @@ const deployTitanNFT: DeployFunction = async function (hre: HardhatRuntimeEnviro
         symbol: "TITAN"
     }
 
+    const FirstEventInfo = {
+        recipient: deployer,
+        priceToken : tonAddress,
+        priceAmount : hre.ethers.utils.parseEther("30")
+    }
     //==== TitanNFT =================================
     const TitanNFTDeployment = await deploy("TitanNFT", {
         from: deployer,
@@ -39,7 +45,7 @@ const deployTitanNFT: DeployFunction = async function (hre: HardhatRuntimeEnviro
     });
 
     // console.log('TitanNFTProxyDeployment', TitanNFTProxyDeployment.address)
-     const proxy = (await hre.ethers.getContractAt(
+    const proxy = (await hre.ethers.getContractAt(
         TitanNFTProxyDeployment.abi,
         TitanNFTProxyDeployment.address
     )) as TitanNFTProxy;
@@ -51,6 +57,34 @@ const deployTitanNFT: DeployFunction = async function (hre: HardhatRuntimeEnviro
         let resetLogic = await proxy.implementation();
         console.log('resetLogic', resetLogic)
     }
+
+
+    //==== FirstEvent =================================
+    const FirstEventDeployment = await deploy("FirstEvent", {
+        from: deployer,
+        args: [deployer],
+        log: true,
+        deterministicDeployment: true,
+    });
+
+    const firstEvent = (await hre.ethers.getContractAt(
+        FirstEventDeployment.abi,
+        FirstEventDeployment.address
+    )) as FirstEvent;
+
+    let nftAddress = await firstEvent.nftAddress();
+    let recipient = await firstEvent.recipient();
+
+    if (nftAddress!= proxy.address || recipient != FirstEventInfo.recipient) {
+        await (await firstEvent.setAddress(proxy.address, FirstEventInfo.recipient)).wait()
+    }
+
+    let priceToken = await firstEvent.priceToken();
+    let priceAmount = await firstEvent.priceAmount();
+    if (priceToken!= FirstEventInfo.priceToken || priceAmount != FirstEventInfo.priceAmount) {
+        await (await firstEvent.setPrice(FirstEventInfo.priceToken, FirstEventInfo.priceAmount)).wait()
+    }
+
 
     //==== verify =================================
 
